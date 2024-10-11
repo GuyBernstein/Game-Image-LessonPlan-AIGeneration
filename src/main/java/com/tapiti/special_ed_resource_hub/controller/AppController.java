4360779b9866a3c4874b4cb07b8ca8d4ff2bd796
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2024-10-11 09:47:42 +0300

    with swagger

---
+++ src/main/java/com/tapiti/special_ed_resource_hub/controller/AppController.java
@@ -0,0 +1,19 @@
+package com.tapiti.special_ed_resource_hub.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+public class AppController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> hello()
+    {
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/com/tapiti/special_ed_resource_hub/SpecialEdResourceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapiti.special_ed_resource_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialEdResourceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
